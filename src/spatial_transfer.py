3900ecb2b5eb9b2bc23970578c9cf4f5b1f4b866
oliverastrand <oliver.astrand@gmail.com>
2019-11-20 12:58:38 -0500

    Improved the code style in spatial_transfer.py

--- src/spatial_transfer.py
+++ src/spatial_transfer.py
@@ -6,35 +6,37 @@ import torchvision.models as models
 
 import copy
 
-from utils import get_image_loader, StyleLoss, ContentLoss, Normalization, imshow
+from .utils import get_image_loader, StyleLoss, ContentLoss, Normalization
 
-class StyleTransfer():
-	"""LEFT: description of class
-	
-	Attributes
-	----------
-	device : torch.device
-		The device on which the model will be used
-	cnn : torch.nn.modules.container.Sequential
-		The used torch module as sequential
-	tractable_layer_names : set of str
-		A set of strings which indicates which layer is a convolution
-	content_layers : list of str
-		The list of which convolutional layers shall be considered in the content loss calculation
-	style_layers : list of str
-		The list of which convolutional layers shall be considered in the style loss calculation
-		
-	Methods
-	-------
-	run_style_transfer(content_path, style_paths, spatial_mask=None, imsize=128, num_steps=300, style_weight=1000000, content_weight=1)
-		Executes style transfer on given image with given style images and masks.
-	get_style_model_and_losses_lists(style_img, content_img, spatial_mask=None)
-		xxx
-	modify_layers(content_layers, style_layers)
-		Sets up the convolutional layers in the network which are used for content and style loss calculation.
-	get_tractable_layer_names():
-		Returns a set of strings which give information which layer are convolutions
-	"""
+
+class StyleTransfer:
+    """A class that transfers multiple styles into one image with the styles spatial intensity given by masks.
+
+    Attributes
+    ----------
+    device : torch.device
+        The device on which the model will be used
+    cnn : torch.nn.modules.container.Sequential
+        The used torch module as sequential
+    tractable_layer_names : set of str
+        A set of strings which indicates which layer is a convolution
+    content_layers : list of str
+        The list of which convolutional layers shall be considered in the content loss calculation
+    style_layers : list of str
+        The list of which convolutional layers shall be considered in the style loss calculation
+
+    Methods
+    -------
+    run_style_transfer(content_path, style_paths, spatial_mask=None, imsize=128, num_steps=300, style_weight=1000000,
+                        content_weight=1)
+        Executes style transfer on given image with given style images and masks.
+    get_style_model_and_losses_lists(style_img, content_img, spatial_mask=None)
+        Returns the model, content and style losses with implemented receptive averaging.
+    modify_layers(content_layers, style_layers)
+        Sets up the convolutional layers in the network which are used for content and style loss calculation.
+    get_tractable_layer_names():
+        Returns a set of strings which give information which layer are convolutions
+    """
 
     def __init__(self, pretrained_model=None, device=None, layers=None):
         
@@ -43,7 +45,6 @@ class StyleTransfer():
         else: 
             self.device = device
         
-        # Prep the model
         if pretrained_model is None:
             
             print("Using default pre-trained network")
@@ -60,14 +61,17 @@ class StyleTransfer():
             
             content_layers = ['conv_4']
             style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4']
-            
+
+            #  Set the style and content layers
+            self.style_layers = []
+            self.content_layers = []
             self.modify_layers(content_layers, style_layers)
             
         else:
             self.modify_layers(layers[0], layers[1])
     
     def run_style_transfer(self, content_path, style_paths, spatial_mask=None, imsize=128, num_steps=300,
-                       style_weight=1000000, content_weight=1):
+                           style_weight=1000000, content_weight=1):
         """Executes style transfer on given image with given style images and masks.
 
         Parameters
@@ -80,12 +84,12 @@ class StyleTransfer():
             The masks which determine in which area of the image which masks shall be applied (default is None)
         imsize : int, optional
             The image size (default is 128)
-		num_steps : int, optional
-			The number of steps which shall be executed for the style transfer (default is 300)
-		style_weight : int, optional
-			The weight of how much the loss in style transfer contributes to the overall loss (default is 1000000)
-		content_weight : int, optional
-			The weight of how much the loss in content preservation contributes to the overall loss (default is 1)
+        num_steps : int, optional
+            The number of steps which shall be executed for the style transfer (default is 300)
+        style_weight : int, optional
+            The weight of how much the loss in style transfer contributes to the overall loss (default is 1000000)
+        content_weight : int, optional
+            The weight of how much the loss in content preservation contributes to the overall loss (default is 1)
         """
         
         image_loader = get_image_loader(imsize, self.device)
@@ -100,7 +104,8 @@ class StyleTransfer():
         
         """Run the style transfer."""
         print('Building the style transfer model..')
-        model, style_losses, content_losses = self.get_style_model_and_losses_lists(style_img, content_img, spatial_mask=spatial_mask)
+        model, style_losses, content_losses = self.get_style_model_and_losses_lists(style_img, content_img,
+                                                                                    spatial_mask=spatial_mask)
         
         optimizer = optim.LBFGS([input_img.requires_grad_()])
 
@@ -143,27 +148,25 @@ class StyleTransfer():
         input_img.data.clamp_(0, 1)
 
         return input_img
-    
-    
+
     def get_style_model_and_losses_lists(self, style_img, content_img, spatial_mask=None):
-		"""Returns the model, content and style losses with implemented receptive averaging.
-		
-		Parameters
-		----------
+        """Returns the model, content and style losses with implemented receptive averaging.
+
+        Parameters
+        ----------
         style_img : list of torch.tensor
             The images whose style shall be transfered
-		content_img : torch.tensor
-			The image on which the style transfer shall be executed
+        content_img : torch.tensor
+            The image on which the style transfer shall be executed
         spatial_mask : list torch.tensor, optional
             The masks which determine in which area of the image which masks shall be applied (default is None)
-		"""
+        """
     
         if spatial_mask is None:
             raise ValueError("A spatial mask must be provided")
 
         if len(spatial_mask) != len(style_img):
             raise ValueError("Number of spatial masks and number of style images must be equal")
-
 
         style_img = torch.stack(style_img).squeeze(1)
 
@@ -172,8 +175,7 @@ class StyleTransfer():
         # normalization module
         normalization = Normalization().to(self.device)
 
-        # just in order to have an iterable access to or list of content/syle
-        # losses
+        # just in order to have an iterable access to or list of content/syle losses
         content_losses = []
         style_losses = []
 
@@ -181,59 +183,39 @@ class StyleTransfer():
         # to put in modules that are supposed to be activated sequentially
         model = nn.Sequential(normalization)
 
-        # We create a receptive averaging network whose input is the mask
-        #receptive_averaging = nn.Sequential()#mask)
-
         # A list with the spatial masks for the style loss
-        receptive_masks = [ [] for k in range(len(style_img)) ]
         receptive_masks = []
         layer_mask = spatial_mask
         receptive_masks.append(layer_mask)
 
         i = 0  # increment every time we see a conv
         for layer in cnn.children():
-            r_layer = None
             if isinstance(layer, nn.Conv2d):
                 i += 1
                 name = 'conv_{}'.format(i)
 
-                #r_name = 'conv_{}'.format(i)
-                #r_layer = nn.Conv2d()
-                #nn.AvgPool2d(layer.kernel_size,layer.stride)
-                #layer_mask = get_new_mask(layer_mask, 'Conv', layer.kernel_size, layer.stride, layer.padding)
-
-    ### how did you find that?
-                r_avg = nn.AvgPool2d(layer.kernel_size,layer.stride, layer.padding)
-                #layer_mask = r_avg(layer_mask.unsqueeze(0)).squeeze(0)
+                r_avg = nn.AvgPool2d(layer.kernel_size, layer.stride, layer.padding)
                 layer_mask = r_avg(layer_mask)
 
             elif isinstance(layer, nn.ReLU):
                 name = 'relu_{}'.format(i)
-                # The in-place version doesn't play very nicely with the ContentLoss
-                # and StyleLoss we insert below. So we replace with out-of-place
-                # ones here.
+
                 layer = nn.ReLU(inplace=False)
+
             elif isinstance(layer, nn.MaxPool2d):
                 name = 'pool_{}'.format(i)
 
-                #r_name = 'pool_{}'.format(i)
-                #r_layer = layer
-                #layer_mask = get_new_mask(layer_mask, 'MaxPooling', layer.kernel_size, layer.stride, layer.padding)
-                r_max = nn.MaxPool2d(layer.kernel_size,layer.stride, layer.padding)
-                #layer_mask = r_max(layer_mask.unsqueeze(0)).squeeze(0)
+                r_max = nn.MaxPool2d(layer.kernel_size, layer.stride, layer.padding)
+
                 layer_mask = r_max(layer_mask)
 
             elif isinstance(layer, nn.BatchNorm2d):
                 name = 'bn_{}'.format(i)
+
             else:
                 raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
 
             model.add_module(name, layer)
-
-            #if r_layer is not None:
-            #    layer_mask = r_layer(layer_mask)
-            #    receptive_masks.append(layer_mask)
-                #receptive_averaging.add_module(r_name, r_layer)
 
             if name in self.content_layers:
                 # add content loss:
@@ -242,15 +224,9 @@ class StyleTransfer():
                 model.add_module("content_loss_{}".format(i), content_loss)
                 content_losses.append(content_loss)
 
-            if name in self.style_layers:            
-
-                # add style loss:
-                target_feature = []
+            if name in self.style_layers:
 
                 target_feature = model(style_img).detach()
-
-                #for k in range(len(style_img)):
-                #    target_feature.append(model(style_img[k]).detach())
 
                 style_loss = StyleLoss(target_feature, layer_mask)
                 model.add_module("style_loss_{}".format(i), style_loss)
@@ -262,24 +238,23 @@ class StyleTransfer():
                 break
         model = model[:(i + 1)]
 
-
         return model, style_losses, content_losses
     
     def modify_layers(self, content_layers, style_layers):
-	"""Sets up the convolutional layers in the network which are used for content and style loss calculation.
-	
-	Parameters
-	----------
-	content_layers : list of str
-		The list of which convolutional layers shall be regarded in the content loss calculation
-	style_layers : list of str
-		The list of which convolutional layers shall be regarded in the style loss calculation
-	
-	Raises
-	------
-	ValueError
-		If any of specified content or style layers is not in the network
-	"""
+        """Sets up which convolutional layers in the network which are used for content and style loss calculation.
+
+        Parameters
+        ----------
+        content_layers : list of str
+            The list of which convolutional layers shall be regarded in the content loss calculation
+        style_layers : list of str
+            The list of which convolutional layers shall be regarded in the style loss calculation
+
+        Raises
+        ------
+        ValueError
+            If any of specified content or style layers is not in the network
+        """
         
         if any([l not in self.tractable_layer_names for l in content_layers]):
             raise ValueError("One of content layers not in network")
@@ -290,27 +265,16 @@ class StyleTransfer():
         self.style_layers = style_layers
         
     def get_tractable_layer_names(self):
-		"""Returns a set of strings which give information which layer are convolutions """
-        
+        """Returns a set of strings which give information which layers in the network
+        can be  used for style transfer """
+
         names = set()
         
         i = 0  # increment every time we see a conv
         for layer in self.cnn.children():
-            r_layer = None
             if isinstance(layer, nn.Conv2d):
                 i += 1
                 name = 'conv_{}'.format(i)
                 names.add(name)
-
-            elif isinstance(layer, nn.ReLU):
-                name = 'relu_{}'.format(i)
-
-            elif isinstance(layer, nn.MaxPool2d):
-                name = 'pool_{}'.format(i)
-
-            elif isinstance(layer, nn.BatchNorm2d):
-                name = 'bn_{}'.format(i)
-            else:
-                raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
             
         return names
