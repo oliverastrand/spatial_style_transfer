fcf9a748b262263f8552535dca8b1cc11744e0a1
oliverastrand <oliver.astrand@gmail.com>
2019-11-20 12:30:52 -0500

    Improved  code style of masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,6 @@
 import torch
-import math
+from math import ceil, floor
+
 
 def get_circular_mask(imsize, center_x, center_y, radius):
     """Returns a mask in form of a PyTorch tensor which has ones in the circle at the specified position.
@@ -18,134 +19,84 @@ def get_circular_mask(imsize, center_x, center_y, radius):
     
     mask = torch.zeros(imsize, imsize)
     
-    for i in range(imsize):
-        for j in range(imsize):
-            if (i - center_x)**2 + (j - center_y)**2 <= (radius**2):
-                mask[j,i] = 1
+    for x in range(imsize):
+        for y in range(imsize):
+            if (x - center_x)**2 + (y - center_y)**2 <= (radius**2):
+                # The first dim of a tensor is vertical and the second horizontal
+                mask[y, x] = 1
                 
     return mask
 
-def get_upper_right_corner_mask(imsize):
-    """Returns a mask in form of a PyTorch tensor which has ones in the upper right corner.
+
+def get_corner_mask(imsize, corner='upper_right'):
+    """Returns a mask in form of a PyTorch tensor which has ones in the corner specified by 'corner'.
     
-    If imagesize is odd, the upper right corner will have size: math.ceil(imsize/2) by math.ceil(imsize/2).
+    If imagesize is odd, the upper right corner will have size: ceil(imsize/2) by ceil(imsize/2).
     
     Parameters
     ----------
     imsize : int
         The size of the image
+    corner : str
+        The corner in which to apply the mask:  'upper_right', 'upper_left',  'lower_left', 'lower_right'
     """
     
-    m1 = torch.ones(( math.ceil(imsize/2) , math.ceil(imsize/2) ))
-    m2 = torch.zeros(( math.ceil(imsize/2), math.ceil(imsize/2) ))
-    
-    mask1 = torch.cat((m2, m1), dim=1)
-    # if imsize is odd, need to cut off first column to match dimension
-    if imsize % 2 ==1:
-        mask1=mask1.narrow(1,1,imsize)
-    
-    mask2 = torch.zeros(( math.floor(imsize/2) , imsize ))
-    
-    return torch.cat((mask1,mask2),dim=0)
-
-def get_upper_left_corner_mask(imsize):
-    """Returns a mask in form of a PyTorch tensor which has ones in the upper left corner.
-    
-    If imagesize is odd, the upper left corner will have size: math.ceil(imsize/2) by math.ceil(imsize/2).
-    
-    Parameters
-    ----------
-    imsize : int
-        The size of the image
-    """
-            
-    m1 = torch.ones(( math.ceil(imsize/2) , math.ceil(imsize/2) ))
-    m2 = torch.zeros(( math.ceil(imsize/2), math.ceil(imsize/2) ))
-    
-    mask1 = torch.cat((m1, m2), dim=1)
-    # if imsize is odd, need to cut off last column to match dimension
-    if imsize % 2 == 1:
-        mask1 = mask1.narrow(1,0,imsize)
-    
-    mask2 = torch.zeros(( math.floor(imsize/2) , imsize ))
-    
-    return torch.cat((mask1,mask2),dim=0)
-
-
-def get_lower_left_corner_mask(imsize):
-    """Returns a mask in form of a PyTorch tensor which has ones in the lower left corner.
-    
-    If imagesize is odd, the lower left corner will have size: math.ceil(imsize/2) by math.ceil(imsize/2).
-    
-    Parameters
-    ----------
-    imsize : int
-        The size of the image
-    """
-    
-    m1 = torch.ones(( math.ceil(imsize/2) , math.ceil(imsize/2) ))
-    m2 = torch.zeros(( math.ceil(imsize/2), math.ceil(imsize/2) ))
-    
-    mask1 = torch.cat((m1, m2), dim=1)
-    # if imsize is odd, need to cut off last column to match dimension
-    if imsize % 2 == 1:
-        mask1 = mask1.narrow(1, 0, imsize)
-    
-    mask2 = torch.zeros(( math.floor(imsize/2) , imsize ))
-
-    return torch.cat((mask2,mask1),dim=0)
-
-def get_lower_right_corner_mask(imsize):
-    """Returns a mask in form of a PyTorch tensor which has ones in the lower right corner.
-    
-    If imagesize is odd, the lower right corner will have size: math.ceil(imsize/2) by math.ceil(imsize/2).
-    
-    Parameters
-    ----------
-    imsize : int
-        The size of the image
-    """
-        
-    m1 = torch.ones(( math.ceil(imsize/2) , math.ceil(imsize/2) ))
-    m2 = torch.zeros(( math.ceil(imsize/2), math.ceil(imsize/2) ))
+    m1 = torch.ones((ceil(imsize/2), ceil(imsize/2)))
+    m2 = torch.zeros((ceil(imsize/2), ceil(imsize/2)))
     
     mask1 = torch.cat((m2, m1), dim=1)
     # if imsize is odd, need to cut off first column to match dimension
     if imsize % 2 == 1:
-        mask1 = mask1.narrow(1,1,imsize)
+        mask1 = mask1.narrow(1, 1, imsize)
+    
+    mask2 = torch.zeros((floor(imsize/2), imsize))
 
-    mask2 = torch.zeros(( math.ceil(imsize/2) , imsize ))
+    mask = torch.cat((mask1, mask2), dim=0)
 
-    return torch.cat((mask2,mask1),dim=0)
+    # Rotate to the right corner
+    if corner == 'upper_right':
+        pass
+    elif corner == 'upper_left':
+        mask = mask.flip(dims=1)
+    elif corner == 'lower_left':
+        mask = mask.transpose(0, 1)
+    elif corner == 'lower_right':
+        mask = mask.flip(dims=0)
+    else:
+        raise ValueError(f'Corner: {corner} not supported')
+
+    return mask
 
 
-def get_left_side_mask(imsize):
-    """Returns a mask in form of a PyTorch tensor which has ones in the left side.
+def get_side_mask(imsize, side='left'):
+    """Returns a mask in form of a PyTorch tensor which has ones on the side specified by 'side'.
     
     Parameters
     ----------
     imsize : int
         The size of the image
+    side : str
+        The side on which to apply the mask: 'left', 'right', 'up', 'down'
     """
     
-    mask1 = torch.ones(imsize, math.ceil(imsize/2))
-    mask2 = torch.zeros(imsize, math.ceil(imsize/2))
-    
-    return torch.cat((mask1,mask2),dim=1)
+    mask1 = torch.ones(imsize, ceil(imsize/2))
+    mask2 = torch.zeros(imsize, ceil(imsize/2))
 
-def get_right_side_mask(imsize):
-    """Returns a mask in form of a PyTorch tensor which has ones in the right side.
+    mask = torch.cat((mask1, mask2), dim=1)
+
+    if side == 'left':
+        pass
+    elif side == 'right':
+        mask = mask.flip(dims=1)
+    elif side == 'up':
+        mask = mask.transpose(0, 1)
+    elif side == 'down':
+        mask = mask.transpose(0, 1).flip(dims=0)
+    else:
+        raise ValueError(f'Side: {side} not supported')
     
-    Parameters
-    ----------
-    imsize : int
-        The size of the image
-    """
-    
-    mask1 = torch.ones(imsize, math.ceil(imsize/2))
-    mask2 = torch.zeros(imsize, math.ceil(imsize/2))
-    
-    return torch.cat((mask2,mask1),dim=1)
+    return mask
+
     
 def get_elliptic_mask(imsize, center_x, center_y, width, height):
     """Returns a mask in form of a PyTorch tensor which has ones in the specified ellipse.
@@ -166,12 +117,13 @@ def get_elliptic_mask(imsize, center_x, center_y, width, height):
     
     mask = torch.zeros(imsize, imsize)
     
-    for i in range(imsize):
-        for j in range(imsize):
-            if ((i-center_x)/(width/2))**2 + ((j-center_y)/(height/2))**2 <= 1:
-                mask[j,i] = 1
+    for x in range(imsize):
+        for y in range(imsize):
+            if ((x-center_x)/(width/2))**2 + ((y-center_y)/(height/2))**2 <= 1:
+                mask[y, x] = 1
     
     return mask
+
 
 def get_rectangular_mask(imsize, upper_left_point_x, upper_left_point_y, length_x, length_y):
     """Returns a mask in form of a PyTorch tensor which has ones in the specified rectangle.
@@ -189,10 +141,11 @@ def get_rectangular_mask(imsize, upper_left_point_x, upper_left_point_y, length_
     length_y : int
         The length along the y-axis of the rectangle
     """
-    
+
     mask = torch.zeros(imsize, imsize)
-    for i in range(upper_left_point_x, upper_left_point_x+length_x):
-        for j in range(upper_left_point_y, upper_left_point_y+length_y):
-            mask[j,i] = 1                  # since first dimension is x-axis and second dimension is y-axis
+
+    for x in range(upper_left_point_x, upper_left_point_x+length_x):
+        for y in range(upper_left_point_y, upper_left_point_y+length_y):
+            mask[y, x] = 1  # since first dimension is x-axis and second dimension is y-axis
     
     return mask
